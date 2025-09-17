27c5fe2469df161c6ede3289ac725005b289fc35
Jeison Maigual <jeisonzzgames@gmail.com>
2025-09-16 21:06:51 -0500

    GetById users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,4 +8,21 @@ module.exports = {
             .then((user_project) => res.status(200).send(user_project))
             .catch((error) => { res.status(400).send(error); });
     },
+    getById(req, res) {
+
+        console.log(req.params.id);
+        return users
+            .findByPk(req.params.id)
+            .then((users) => {
+                console.log(users);
+                if (!users) {
+                    return res.status(404).send({
+                        message: 'users Not Found',
+                    });
+                }
+                return res.status(200).send(users);
+            })
+            .catch((error) =>
+                res.status(400).send(error));
+    },
 };
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const usersController = require('../controllers').usersController;
 
 router.get('/', usersController.list);
+router.get('/:id', usersController.getById);
 
 module.exports = router;
